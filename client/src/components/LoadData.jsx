9ea9ef4312d21e68a4791471b371e9a3346128e4
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2025-01-08 21:18:57 +0200

    Clean up the code from Vite  default

--- client/src/components/LoadData.jsx
+++ client/src/components/LoadData.jsx
@@ -72,11 +72,11 @@ const LoadData = () => {
 
         {/* Loading Animation */}
         {loading ? (
-          <div class="text-center">
+          <div className="text-center">
             <div role="status">
               <svg
                 aria-hidden="true"
-                class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -90,7 +90,7 @@ const LoadData = () => {
                   fill="currentFill"
                 />
               </svg>
-              <span class="sr-only">Loading data...</span>
+              <span className="sr-only">Loading data...</span>
             </div>
           </div>
         ) : cityData.length === 0 ? (
