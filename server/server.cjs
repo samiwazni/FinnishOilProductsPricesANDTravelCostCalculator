c4f2a69315d6324ceffd9e0b9c4e719b3d3e8ee1
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2025-01-12 16:35:44 +0200

    Clean and format server file

--- server/server.cjs
+++ server/server.cjs
@@ -1,13 +1,17 @@
-const express = require('express');
-const scrapeRoutes = require('./routes/scrapeRoutes.cjs');
-const cors = require('cors');
+/*  File name: server.cjs
+    Desc: This is the server main file
+*/
+// Import necessary modules
+const express = require("express");
+const scrapeRoutes = require("./routes/scrapeRoutes.cjs");
+const cors = require("cors");
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 
-app.use('/api', scrapeRoutes);
+app.use("/api", scrapeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
