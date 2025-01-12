875f04a818c2b076e04fbecca954fd784e62708a
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2025-01-12 16:32:37 +0200

    Format file

--- client/src/api.js
+++ client/src/api.js
@@ -1,18 +1,18 @@
-const API_URL = 'http://localhost:5000/api/fuelprices'; // URL -> Backend API
+const API_URL = "http://localhost:5000/api/fuelprices"; // URL -> Backend API
 
 // Function to fetch fuel prices and the last update timestamp
 export const fetchFuelData = async () => {
   try {
     const response = await fetch(API_URL);
     const data = await response.json();
-    
+
     if (response.ok) {
-      return data;  // Return data which contains 'lastUpdate' and 'fuelPrices'
+      return data; // Return data which contains 'lastUpdate' and 'fuelPrices'
     } else {
-      throw new Error('Error fetching fuel data');
+      throw new Error("Error fetching fuel data");
     }
   } catch (error) {
-    console.error('Failed to fetch fuel data:', error);
+    console.error("Failed to fetch fuel data:", error);
     throw error;
   }
 };
